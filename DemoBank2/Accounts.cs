71d1f32f24f1c1bc4a706de6850d4a532c25b1a3
MarkKithinji <105651925+MarkKithinji@users.noreply.github.com>
2022-11-03 21:22:12 +0300

    Add project files.

---
+++ DemoBank2/Accounts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoBank2
+{
+    internal class Accounts
+    {
+        public class bankAccount
+        {
+            public string bankID;
+            public string bankName;
+            public string customerName;
+            public string customerID;
+            public string deposit;
+            public string balance;
+
+
+        }
+        public void newCustomer(string name, int age, string password, int deposit)
+        {
+
+        }
+    }
+}
---
+++ DemoBank2/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool repeat = false;
+
+
+        do { 
+        Console.WriteLine("AUTHENTICATION:\n> Press C for customer and S for staff");
+        string AutResponse = Console.ReadLine();
+        
+
+        if (AutResponse.ToUpper() == "C")
+        {
+            Console.WriteLine(AutResponse);
+        }
+        else if (AutResponse.ToUpper() == "S")
+        {
+            Console.WriteLine("what should we do today\n1.Signup someone new!\n2. Get customer data\n3. Clear/Remove account");
+                repeat = false;
+
+                switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                    case int option when option == 1:
+                        Console.WriteLine("CUSTOMER SIGNUP");
+                        for (int i=0; i<=4; i++)
+                        {
+                            Console.WriteLine();
+                            do
+                            {
+
+                                int j = 0;
+                                if (j > 0) { Console.WriteLine("Null Response mot allowed, Retry"); }
+                                string[] options = new string[5] { "name", "age", "password", "password again", "deposit" };
+                                string[] exportOptions = new string[5];
+                                Console.WriteLine($"Enter the customer's {options[i]} :");
+                                if (Console.ReadLine() != "") { exportOptions[i] = Console.ReadLine(); }
+                                j++;
+                            }
+                            while (Console.ReadLine() == "");
+
+
+                        }
+
+                        break;
+                    case int option when option == 2:
+                        Console.WriteLine(AutResponse);
+                        break;
+                    case int option when option == 3:
+                        Console.WriteLine(AutResponse);
+                        break;
+                }
+        }
+        else
+        {
+            Console.WriteLine($"Expected S or C, instead got {AutResponse}; Seriously bro??\n>>DO you wish to retry? Y/N");
+            if (Console.ReadLine() == "Y" || Console.ReadLine == null) {repeat = true; }
+            else {repeat = false;}
+
+        }
+        }
+        while (repeat == true);
+    }
+}
