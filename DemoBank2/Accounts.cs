230a03eef19a2dd207019d75bc38afd663e8e459
MarkKithinji <105651925+MarkKithinji@users.noreply.github.com>
2022-11-04 14:39:35 +0300

    finetuned the new customer class to autogenerate customer ID.

--- DemoBank2/Accounts.cs
+++ DemoBank2/Accounts.cs
@@ -10,17 +10,42 @@ namespace DemoBank2
     {
         public class bankAccount
         {
-            public string bankID;
-            public string bankName;
+            private string bankID = "254-12-2034";
+            private string bankName = "National Bank - Meru";
             public string customerName;
             public string customerID;
             public string deposit;
             public string balance;
+            private string password;
 
+            public string BankID
+            {
+                get { return bankID; }
+            }
+            public string BankName
+            {
+                get { return bankName; }
+            }
+            public string Password { set { password = value; } }
 
         }
-        public void newCustomer(string name, int age, string password, int deposit)
+        public void newCustomer(string name, string age, string password, string deposit)
         {
+            Console.WriteLine("response"+ name + age+ password+ deposit);
+            int newAge = int.Parse(age);
+            int newPassword = int.Parse(password);
+            int newDeposit = int.Parse(deposit);
+            string part1ID, part2ID, part3ID;
+            if (newAge < 18) { part1ID = "Ch"; }
+            else if( newAge>= 18 && newAge <=60) { part1ID = "Ud"; }
+            else if(newAge> 40) { part1ID = "Re"; }
+
+            if (newDeposit < 10000) { part2ID = "C"; }
+            else if (newDeposit >= 10000 && newDeposit <= 70000) { part2ID = "B"; }
+            else if (newDeposit > 70000 && newDeposit <= 200000) { part2ID = "A"; }
+            else if (newDeposit > 200000) { part2ID = "Plat"; }
+
+            //get the customer a new ID
 
         }
     }
--- DemoBank2/Program.cs
+++ DemoBank2/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using DemoBank2;
 using System;
+using System.Security.Principal;
 
 internal class Program
 {
@@ -27,24 +29,28 @@ internal class Program
                     {
                     case int option when option == 1:
                         Console.WriteLine("CUSTOMER SIGNUP");
+
+                        string[] exportOptions = new string[6];
                         for (int i=0; i<=4; i++)
                         {
                             Console.WriteLine();
-                            do
+                            int j = 0;
+                            if (j > 0) { Console.WriteLine("Null Response mot allowed, Retry"); }
+                            string[] options = new string[5] { "name", "age", "password", "password again", "deposit" };
+
+                            Console.WriteLine($"Enter the customer's {options[i]} :");
+                            exportOptions[i] = Console.ReadLine();
+
+                            if (exportOptions[2] != exportOptions[3] && i == 3)
                             {
-
-                                int j = 0;
-                                if (j > 0) { Console.WriteLine("Null Response mot allowed, Retry"); }
-                                string[] options = new string[5] { "name", "age", "password", "password again", "deposit" };
-                                string[] exportOptions = new string[5];
-                                Console.WriteLine($"Enter the customer's {options[i]} :");
-                                if (Console.ReadLine() != "") { exportOptions[i] = Console.ReadLine(); }
-                                j++;
+                                Console.WriteLine($"CRITICAL ERROR!\n>>Passwords {exportOptions[2]} and {exportOptions[3]} did not match.\n>>Process terminated");
+                                break;
                             }
-                            while (Console.ReadLine() == "");
-
+                               
 
                         }
+                        Accounts account = new Accounts();
+                        account.newCustomer(exportOptions[0], exportOptions[1], exportOptions[2], exportOptions[4]);
 
                         break;
                     case int option when option == 2:
